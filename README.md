d9968cfce1ec1ae28ade50507efcc631aa3b0b7e
Vitaly Lobchuk <vitaly@lobchuk.ru>
2017-03-29 14:28:21 +0300

    feat(all): initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Tool can refactoring imports
+============================
+
+# Install 
+
+``` golang
+$ go get github.com/l-vitaly/goimportrefactor/...
+```
+
+# Usage
+
+Add you $GOPATH/bin in your PATH env.
+
+```
+goimportrefactor -form=github.com/username/package -to=gitlab.company.com/username/package
+```
---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"golang.org/x/tools/go/ast/astutil"
+)
+
+var (
+	fromFlag = flag.String("from", "", "identifier to be renamed")
+	toFlag   = flag.String("to", "", "new name for identifier")
+	debug    = flag.Bool("debug", false, "debug mode")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	pkgs, _ := dirPackageInfo("./", *fromFlag, *toFlag)
+	for _, pi := range pkgs {
+		refactImports(pi.fset, pi.f, pi.paths, pi.filename, *debug)
+	}
+}
+
+type packageInfo struct {
+	filename string
+	f        *ast.File
+	fset     *token.FileSet
+	paths    []*pathInfo
+}
+
+type pathInfo struct {
+	old, new string
+}
+
+// refactImports fixes imports to new.
+func refactImports(
+	fset *token.FileSet, f *ast.File, paths []*pathInfo, filename string, debug bool,
+) error {
+	abs, err := filepath.Abs(filename)
+	if err != nil {
+		return err
+	}
+	srcDir := filepath.Dir(abs)
+
+	if len(paths) > 0 {
+		var changed bool
+
+		for _, path := range paths {
+			if !astutil.RewriteImport(fset, f, path.old, path.new) {
+				continue
+			}
+			if debug {
+				log.Printf("refactImports(newName=%q), abs=%q, srcDir=%q\n", path.new, abs, srcDir)
+			}
+			changed = true
+		}
+
+		if changed {
+			var buf bytes.Buffer
+			if err := format.Node(&buf, fset, f); err != nil {
+				return fmt.Errorf("%s: couldn't format file: %v", filename, err)
+			}
+			ioutil.WriteFile(filename, buf.Bytes(), 0755)
+		}
+
+	}
+	return nil
+}
+
+// dirPackageInfo find old imports
+func dirPackageInfo(srcDir string, from string, to string) ([]*packageInfo, error) {
+	re, _ := regexp.Compile(from)
+
+	packageFileInfos, err := ioutil.ReadDir(srcDir)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []*packageInfo
+	for _, fi := range packageFileInfos {
+		if !strings.HasSuffix(fi.Name(), ".go") {
+			continue
+		}
+
+		fileSet := token.NewFileSet()
+		root, err := parser.ParseFile(fileSet, filepath.Join(srcDir, fi.Name()), nil, 0)
+		if err != nil {
+			continue
+		}
+
+		var paths []*pathInfo
+		for _, decl := range root.Decls {
+			genDecl, ok := decl.(*ast.GenDecl)
+			if !ok {
+				continue
+			}
+			for _, spec := range genDecl.Specs {
+				valueSpec, ok := spec.(*ast.ImportSpec)
+				if !ok {
+					continue
+				}
+				if re.Match([]byte(valueSpec.Path.Value)) {
+					res := re.ReplaceAll([]byte(valueSpec.Path.Value), []byte(to))
+					paths = append(paths, &pathInfo{old: strings.Trim(valueSpec.Path.Value, `""`), new: strings.Trim(string(res), `""`)})
+				}
+			}
+		}
+		result = append(result, &packageInfo{filename: fi.Name(), fset: fileSet, f: root, paths: paths})
+	}
+	return result, nil
+}
